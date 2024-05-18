344c163abb15b5e58a429b83e16a813c51bca7a7
pedrodanielbrunetto <pedrodanielbm@hotmail.com>
2024-05-18 17:51:32 -0300

    feat: add column in Ideas.js

--- src/Services/Ideas.js
+++ src/Services/Ideas.js
@@ -1,17 +1,19 @@
 const pool = require("../../Infra/Connection");
+const moment = require('moment-timezone');
 
 const Ideas = async (req, res) => {
   const { name, email, cel, idea } = req.body;
-
+  const datetimeBR = moment.tz("America/Sao_Paulo").format('YYYY-MM-DD HH:mm:ss');
+  
   console.log('Registrando nova ideia com os seguintes dados:', { name, email, cel, idea });
 
   try {
     let registerIdea = `
-      INSERT INTO FutureClientsIdeas (Name, Email, Celphone, Idea) 
-      VALUES (?, ?, ?, ?)
+      INSERT INTO FutureClientsIdeas (Name, Email, Celphone, Idea, Data_Registro) 
+      VALUES (?, ?, ?, ?, ?)
     `;
 
-    await pool.query(registerIdea, [name, email, cel, idea], (error, results) => {
+    await pool.query(registerIdea, [name, email, cel, idea, datetimeBR], (error, results) => {
       if (error) {
         console.error('Erro ao registrar a ideia:', error.stack);
         return res.status(500).json({ error: 'Erro interno do servidor' });
